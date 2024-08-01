d5952824ff49c81d4e4ef190222e2b90e1177139
root <root@DESKTOP-Q5OD9L9>
2024-08-02 03:57:34 +0800

    fixed front 2

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Product Catalog</title>
+    <title>Каталог товаров</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .expand-on-hover {
